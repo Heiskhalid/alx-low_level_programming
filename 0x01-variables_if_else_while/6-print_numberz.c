335c495c155c2ba64cddd0fb7355dabe67d6f823
Heiskhalid <khaliddolapo@gmail.com>
2022-10-14 17:29:01 -0700

    polo

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+
+
+
+
+
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+        int c;
+
+        c = 0;
+        while
+                (c < 10) {
+                        putchar(c + '0');
+                        c++;
+                }
+        putchar('\n');
+        return (0);
+}
