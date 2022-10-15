234dbf247b16de35c72d55e60979f48af93ec0de
Khalid Adewale <99570474+Heiskhalid@users.noreply.github.com>
2022-10-15 02:23:05 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 
 
 /**
- *	main - prints the lowercase alphabet in reverse,
+ *	 main - prints all the numbers of base 16 in lowercase,
  *
  *	followed by a new line
  *
@@ -14,13 +14,21 @@
 int main(void)
 
 {
+int n;
+
 char ch;
 
-for (ch = 'z'; ch >= 'a'; ch--)
+for (n = 48; n < 58; n++)
+{
+
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
 {
 putchar(ch);
-	}
+}
 putchar('\n');
+
 return (0);
 
 }
