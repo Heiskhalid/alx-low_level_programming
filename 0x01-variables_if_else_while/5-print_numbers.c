3bda185eea6b9737a79650ec504db2533024ac08
Khalid Adewale <99570474+Heiskhalid@users.noreply.github.com>
2022-10-15 02:12:15 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,9 +3,9 @@
 
 
 /**
- *	main - prints all single digit numbers of base
+ *	main - prints all single digit numbers of base 10 starting from 0,
  *
- *	10 starting from 0, followed by a new line
+ *	followed by a new line, using putchar
  *
  *	Return: Always 0 (Success)
  *
@@ -14,12 +14,15 @@
 int main(void)
 
 {
+
 int n;
-for (n = 0; n < 10; n++)
 
-printf("%d", n);
+for (n = 48; n < 58; n++)
+{
 
-printf("\n");
+putchar(n);
+	}
+putchar('\n');
 
 return (0);
 
