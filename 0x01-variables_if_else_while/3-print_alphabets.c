4409ff8e4c0c461e14edc0352bc2120dbfcfb7d4
Khalid Adewale <99570474+Heiskhalid@users.noreply.github.com>
2022-10-15 02:20:38 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,10 @@ int main(void)
 
 {
 
-char alp[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGJIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGJIJKLMNOPQRSTUVWXYZ";
 
 int i;
-for (i = 0; i < 26; i++)
+for (i = 0; i < 52; i++)
 {
 putchar(alp[i]);
 }
